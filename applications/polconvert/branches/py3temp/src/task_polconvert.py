difx/difx-svn2git-try6

commit 6c07b0f04b17d2381f0c26c36add3874d2910e97

fix PolConvert Error in task_polconvert.py occurring for scans with 100+ freq table entries, i.e. parse DiFX key-value lines from index 100 onwards such as 'OVERSAMPLE FAC. 100:1' correctly into value '1' rather than value '100:1'
git-svn-id: https://svn.atnf.csiro.au/difx@10287 9d4a3555-dc7e-4673-bb7d-d8a0b9a30876

applications/polconvert/branches/py3temp/src/task_polconvert.py
@@ -1159,7 +1159,7 @@ def polconvert(IDI, OUTPUTIDI, DiFXinput, DiFXcalc, doIF, linAntIdx,
      for line in inputlines[fr+1:]:
        if entry in line:
          index = int((line.split(':')[0]).split()[-1])
-         FrInfo[entry][index] = type(FrInfo[entry][0])(line.split()[-1])
+         FrInfo[entry][index] = type(FrInfo[entry][0])(line.split(':')[-1])
 
 # SORT OUT THE CHANNEL FREQUENCIES:
 

applications/polconvert/trunk/src/task_polconvert.py
@@ -1063,7 +1063,7 @@ def polconvert(IDI, OUTPUTIDI, DiFXinput, DiFXcalc, doIF, linAntIdx, Range, ALMA
      for line in inputlines[fr+1:]:
        if entry in line:
          index = int((line.split(':')[0]).split()[-1])
-         FrInfo[entry][index] = type(FrInfo[entry][0])(line.split()[-1])
+         FrInfo[entry][index] = type(FrInfo[entry][0])(line.split(':')[-1])
 
 # SORT OUT THE CHANNEL FREQUENCIES:
 

commit 467acc4505ab7a6f6974833ae586ac0937f2ca55

Completing the fix/bug introduced in r10287.
The original code was fine for <100 entries.  The change
in 10287 fixed the parsing, EXCEPT that it left spaces in
the sideband indicator, later converting a test for 'U'(SB)
into a failure.



git-svn-id: https://svn.atnf.csiro.au/difx@10374 9d4a3555-dc7e-4673-bb7d-d8a0b9a30876

applications/polconvert/branches/py3temp/src/task_polconvert.py
@@ -1159,7 +1159,7 @@ def polconvert(IDI, OUTPUTIDI, DiFXinput, DiFXcalc, doIF, linAntIdx,
      for line in inputlines[fr+1:]:
        if entry in line:
          index = int((line.split(':')[0]).split()[-1])
-         FrInfo[entry][index] = type(FrInfo[entry][0])(line.split(':')[-1])
+         FrInfo[entry][index] = type(FrInfo[entry][0])(line.split(':')[-1].strip())
 
 # SORT OUT THE CHANNEL FREQUENCIES:
 

applications/polconvert/trunk/src/task_polconvert.py
@@ -1063,7 +1063,7 @@ def polconvert(IDI, OUTPUTIDI, DiFXinput, DiFXcalc, doIF, linAntIdx, Range, ALMA
      for line in inputlines[fr+1:]:
        if entry in line:
          index = int((line.split(':')[0]).split()[-1])
-         FrInfo[entry][index] = type(FrInfo[entry][0])(line.split(':')[-1])
+         FrInfo[entry][index] = type(FrInfo[entry][0])(line.split(':')[-1].strip())
 
 # SORT OUT THE CHANNEL FREQUENCIES:
 